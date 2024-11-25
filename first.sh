1ccf090923125c288656e602cc0223ef1dbe5a00
Swopnil <swopnil@example.com>
2024-11-25 12:48:30 +0000

    Sth edited

--- first.sh
+++ first.sh
@@ -2,3 +2,13 @@
 
 
 echo "Hello I am Swopnil"
+
+var1=5
+var2=10
+
+echo "Please enter your name: "
+read name
+echo "Please enter your age: "
+read age
+echo "Your name is $name and your current age is : $age."
+
